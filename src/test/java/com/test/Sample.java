0700100ed4e37d1466055fea9245f717bb275d76
JkAparna <venkatakrishnan.aparna@gmail.com>
2024-08-07 22:57:13 +0530

    first commit

---
+++ src/test/java/com/test/Sample.java
@@ -0,0 +1,18 @@
+package com.test;
+
+
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+public class Sample {
+	@Test
+	public void user() {
+		String s = "Aparna";
+		Assert.assertEquals(false, false);
+		
+		
+	
+	}
+
+}
